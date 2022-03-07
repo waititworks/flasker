bec28c56556a184e0633c4e21eb902e738cd674c
Connor McCarter <connormccarter@sbcglobal.net>
2022-03-06 20:32:59 -0600

    Minor wording fix -500

--- hello.py
+++ hello.py
@@ -41,7 +41,7 @@ def user(name):
 def page_not_found(e):
     return render_template("404.html"), 404
 
-#invalid url
+#internal server error
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
